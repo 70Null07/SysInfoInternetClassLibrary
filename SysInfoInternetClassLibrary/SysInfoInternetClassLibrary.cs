735fa81a42a39d7aa28130cdd28503c6ec21cefc
Дмитрий Барышев <dmitriy_baryshev@list.ru>
2024-05-15 20:57:09 +0700

    Добавьте файлы проекта.

---
+++ SysInfoInternetClassLibrary/SysInfoInternetClassLibrary.cs
@@ -0,0 +1,32 @@
+﻿using PluginBase;
+using System.Net;
+
+namespace SysInfoInternetClassLibrary
+{
+    public class SysInfoInternetCommand : ICommand
+    {
+        public string Name { get => "SysInfoInternet"; }
+
+        public string Description { get => "SysInfoInternet"; }
+
+        public double Version { get => 1.0; }
+
+        public List<string> Execute()
+        {
+            List<string> result = new List<string>();
+
+            string host = Dns.GetHostName();
+
+            result.Add($"Имя компьютера: {host}");
+
+            IPAddress[] addresses = Dns.GetHostAddresses(host);
+
+            foreach (IPAddress address in addresses)
+            {
+                result.Add($"Адрес: {address} Семейство: {address.AddressFamily}");
+            }
+
+            return result;
+        }
+    }
+}
